6af4131664253a31301e06da7d3d705ea654e882
athleticareai-ctrl <athleticareai@gmail.com>
2026-01-11 10:04:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-    res.redirect("about.html");
+    res.redirect("/about.html");
 });
 
 // ================= EMAIL SETUP =================
